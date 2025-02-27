8473e23f7de42f1d1f5918eb8dafe94d42196fd5
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-02-27 20:26:21 -0300

    export css in different styles such as compressed

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ function css(done) {
   // Compile SASS
   // Steps: 1- Identify .scss file, 2- Compile file, 3- Save .css file
 
-  src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
+  src("src/scss/app.scss")
+    .pipe(sass({ style: "compressed" }))
+    .pipe(dest("build/css"));
 
   done();
 }
