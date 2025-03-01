6d21b827e0bc6e25edf2268b8b2e97fe88c43fd7
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-03-01 20:19:35 -0300

    add gulp function for uploading images to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
 function css(done) {
-  // Compile SASS
-  // Steps: 1- Identify .scss file, 2- Compile file, 3- Save .css file
+  /* 
+    Compile SASS
+    Steps: 1- Identify .scss file, 2- Compile file, 3- Save .css file
+  */
 
   src("src/scss/app.scss")
     .pipe(sass())
@@ -15,13 +17,27 @@ function css(done) {
   done();
 }
 
+function images() {
+  return src("src/img/**/*").pipe(dest("build/img"));
+}
+
+// function images(done) {
+//   src("src/img/**/*").pipe(dest("build/img"));
+
+//   done();
+// }
+
 function dev() {
   watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", images);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.images = images;
+exports.default = series(images, css, dev);
 
-// series - a task is started, and until it finishes, the next one starts.
-// parallel -  all tasks start at the same time
+/* 
+  series - a task is started, and until it finishes, the next one starts.
+  parallel -  all tasks start at the same time 
+*/
